d80e777679d48ad17290733ceb5adf226b66e1c7
Piyush K <piyush.l.khadgi@gmail.com>
2020-03-10 14:35:37 +0530

    Piyush : First Commit

--- Skeleton/Data_read.py
+++ Skeleton/Data_read.py
@@ -1,11 +1,25 @@
 import subprocess
 
-project = 'k_titanic'
-root_path =
+project = 'titanic'
 
+folder = 'k_'+project
+file_name = project+'.zip'
+root_path = '/home/swayush/ML/'
 
-out = subprocess.check_output(["ls", "-l"])
+out = subprocess.check_output(["ls",root_path]).decode("utf-8")
+if folder not in out.split('\n'): out = subprocess.check_output(["mkdir",root_path+folder]).decode("utf-8")
+out = subprocess.check_output(["ls"]).decode("utf-8")
+if file_name in out.split('\n'): subprocess.check_output(["rm",file_name]).decode("utf-8")
+out = subprocess.check_output(['kaggle','competitions','download',project]).decode("utf-8")
+out = subprocess.check_output(['cp',file_name,root_path+folder]).decode("utf-8")
+out = subprocess.check_output(["rm",file_name]).decode("utf-8")
+out = subprocess.check_output(["ls",root_path+folder]).decode("utf-8")
+if 'raw_data' not in out.split('\n'): out = subprocess.check_output(["mkdir",root_path+folder+'/raw_data']).decode("utf-8")
+#out = subprocess.check_output(["unzip",root_path+folder+'/'+file_name,'-d '+root_path+folder+'/raw_data').decode("utf-8")
 
+#out = subprocess.check_output(["pwd"]).decode("utf-8")
+#if file_name not in out.split(' '): print('out')
+print('-d '+root_path+folder+'/raw_data')
 
 # todo: Ascertain path exists
 # todo: Download data if not present
