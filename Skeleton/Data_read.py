dbe3458a18ba145c1c6d5a6b74faa46d6eeb448d
Piyush K <piyush.l.khadgi@gmail.com>
2020-03-10 11:14:50 +0530

    Piyush : First Commit

--- Skeleton/Data_read.py
+++ Skeleton/Data_read.py
@@ -1,2 +1,30 @@
 import subprocess
-out = subprocess.call(["ls", "-l"])
+
+project = 'k_titanic'
+root_path =
+
+
+out = subprocess.check_output(["ls", "-l"])
+
+
+# todo: Ascertain path exists
+# todo: Download data if not present
+# todo: Unzip and create final file
+
+# todo: Feature understanding
+# todo: Missing Value
+# todo: Feature engineering
+# todo: Feature understanding
+# todo: Sampling code
+
+# todo: Train
+# todo: validate
+# todo: predict
+# todo: Store prediction
+# todo: Submit
+
+
+
+
+
+
