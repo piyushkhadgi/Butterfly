b64a1ff3c2dc0254101df9a75c741a5225ba9663
Piyush K <piyush.l.khadgi@gmail.com>
2020-03-12 23:38:18 +0530

    Piyush : Bones is final

--- Bones.py
+++ Bones.py
@@ -23,24 +23,25 @@ def Read(Config):
     if Config.Source == 'kaggle':
         Folder = 'k_' + Config.Project
         FileName = Config.Project + '.zip'
+        Location = Config.RootPath + Folder
 
         Out = subprocess.check_output(["ls", Config.RootPath]).decode("utf-8")
         if Folder not in Out.split('\n'):
-            subprocess.check_output(["mkdir", Config.RootPath + Folder]).decode("utf-8")
+            subprocess.check_output(["mkdir", Location]).decode("utf-8")
         Out = subprocess.check_output(["ls"]).decode("utf-8")
         if FileName in Out.split('\n'):
             subprocess.check_output(["rm", FileName]).decode("utf-8")
         subprocess.check_output(['kaggle', 'competitions', 'download', Config.Project]).decode("utf-8")
-        subprocess.check_output(['cp', FileName, Config.RootPath + Folder]).decode("utf-8")
+        subprocess.check_output(['cp', FileName, Location]).decode("utf-8")
         subprocess.check_output(["rm", FileName]).decode("utf-8")
-        Out = subprocess.check_output(["ls", Config.RootPath + Folder]).decode("utf-8")
+        Out = subprocess.check_output(["ls", Location]).decode("utf-8")
         if 'raw_data' in Out.split('\n'):
-            Out = subprocess.check_output(["rm", '-r', Config.RootPath + Folder + '/raw_data']).decode("utf-8")
-        subprocess.check_output(["mkdir", Config.RootPath + Folder + '/raw_data']).decode("utf-8")
-        subprocess.check_output(["unzip", Config.RootPath + Folder + '/' + FileName, '-d', Config.RootPath + Folder + '/raw_data']).decode("utf-8")
-        Df_trn = pandas.read_csv(Config.RootPath + Folder + '/raw_data/train.csv')
+            Out = subprocess.check_output(["rm", '-r', Location + '/raw_data']).decode("utf-8")
+        subprocess.check_output(["mkdir", Location + '/raw_data']).decode("utf-8")
+        subprocess.check_output(["unzip", Location + '/' + FileName, '-d', Location + '/raw_data']).decode("utf-8")
+        Df_trn = pandas.read_csv(Location + '/raw_data/train.csv')
         Df_trn['_data_'] = 'train'
-        Df_tst = pandas.read_csv(Config.RootPath + Folder + '/raw_data/test.csv')
+        Df_tst = pandas.read_csv(Location + '/raw_data/test.csv')
         Df_tst['_data_'] = 'test'
         Df = Df_trn.append(Df_tst, ignore_index=True)
         Df.to_csv(Config.RootPath + Folder+'/raw.csv',index=False)
