f5b70854bbde1cb8440015cc85577f75150f5829
Piyush K <piyush.l.khadgi@gmail.com>
2020-04-11 23:33:01 +0530

    Piyush : Bones is final

--- ML.py
+++ ML.py
@@ -11,20 +11,24 @@ def DT_cls(config):
     DT.fit(df.loc[df._data_ == 1].drop(config.target, axis=1).drop(config.primary, axis=1), df.loc[df._data_ == 1,[config.target]])
     prediction = pandas.DataFrame(DT.predict_proba(df.loc[df._data_ == 0].drop(config.target, axis=1).drop(config.primary, axis=1))[:, 1])
     prediction.columns = ['prob']
-    prediction['source'] = 'SKL_DT'
     prediction[config.primary] = df.loc[df._data_ == 0].index
 
     prediction.loc[prediction['prob'] <= config.threshold,config.primary] = 0
     prediction.loc[prediction['prob'] > config.threshold,config.primary] = 1
 
-    out = subprocess.check_output(["ls",config.result_file]).decode("utf-8")
+    out = subprocess.check_output(["ls",config.proj_path]).decode("utf-8")
     if 'result.csv' in out.split('\n'):
         res = pandas.read_csv(config.result_file, delimiter=',')
-        temp = pandas.DataFrame(filter(lambda x: x.startswith('SKL_DT_'), res.source.unique()), columns=['source'])
-        temp['series'] = pandas.to_numeric(temp['source'].str[-4:])
-
-        prediction.to_csv(path_or_buf=config.result_file, index=False, mode = 'a', header = False)
+        temp = pandas.DataFrame(filter(lambda x: x.startswith('SKL_DT_'), res.algo.unique()), columns=['algo'])
+        temp['series'] = pandas.to_numeric(temp['algo'].str[-4:])
+        if len(temp.index) == 0:
+            prediction['algo'] = 'SKL_DT_0001'
+            prediction.to_csv(path_or_buf=config.result_file, index=False, mode='a', header=False)
+        else:
+            prediction['algo'] = 'SKL_DT_' + f"{temp.series.max()+1:04}"
+            prediction.to_csv(path_or_buf=config.result_file, index=False, mode = 'a', header = False)
     else:
+        prediction['algo'] = 'SKL_DT_0001'
         prediction.to_csv(path_or_buf=config.result_file, index=False)
 
 
